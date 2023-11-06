5a48c536bf4d373237a306d6479e53fabda75314
Marija Holzeimer <diatmin@gmail.com>
2023-11-06 20:26:03 +0300

    Comb task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,12 +3,12 @@ function solveEquation(a, b, c) {
   const arr = [];
   const d = b**2 - 4 * a * c;
   if (d > 0) {
-    const x1 = (-b + Math.sqrt(d) )/(2*a);
-    const x2 = (-b - Math.sqrt(d) )/(2*a);
+    const x1 = (-b + Math.sqrt(d) ) / (2 * a);
+    const x2 = (-b - Math.sqrt(d) ) / (2 * a);
     arr.push(x1);
     arr.push(x2);
   } else if (d === 0) {
-    const x = -b/(2*a);
+    const x = -b / (2 * a);
     arr.push(x);
   }
   return arr;
