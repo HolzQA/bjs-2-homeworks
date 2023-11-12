fac82bbc3439395e0fb0d3615ee838012518da32
Marija Holzeimer <diatmin@gmail.com>
2023-11-13 01:54:05 +0300

    Mission complete

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length) {
+        return arr1.every((item, index) => item === arr2[index]);
+    }
+    return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0 && gender != "женский" && gender != "мужской") {
+        return 0;
+    }
+    
+    return users.filter((user) => user.gender === gender).reduce((acc, user, index, arr) => {
+        acc +=user.age;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+    },0);
 }
