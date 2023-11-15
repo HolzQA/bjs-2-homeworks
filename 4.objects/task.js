adb8ac5bcc7ef27faeb6debf819e65980bed70e0
Marija Holzeimer <diatmin@gmail.com>
2023-11-16 01:07:53 +0300

    Something wrong

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  Student.prototype.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (Student.hasOwnProperty("marks")) {
+    Student.marks.push(...marksToAdd);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!Student.hasOwnProperty("marks") || Student.marks.length === 0) {
+    return 0;
+  } else {
+    return Student.marks.reduce((acc, mark, index, arr) => acc + mark / arr.length, 0);
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete Student.subject;
+  delete Student.marks;
+  Student.protoype.excluded = reason;
 }
+
